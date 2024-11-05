74c5488013e1afa30191144357bad75c997381d4
tw20th <51616143+tw20th@users.noreply.github.com>
2024-11-05 14:27:12 +0900

    Add static export configuration for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/my-app", // サブディレクトリのパス
+  trailingSlash: true, // 各ページをフォルダとしてエクスポート
+  output: "export",    // 静的エクスポートを有効化
+  distDir: "docs",     // 出力フォルダを docs に指定
 };
 
 export default nextConfig;
